e835a8db8ee79d40fdfc365e374eaf088a1355e6
Carsten Bach <mail@carsten-bach.de>
2023-09-16 00:10:56 +0200

    WIP (not working)

---
+++ tests/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Example test class
+ *
+ * @package wpacceptance
+ * @todo #16 Establish Acceptance testing
+ */
+
+/**
+ * PHPUnit test class
+ */
+class ExampleTest extends \WPAcceptance\PHPUnit\TestCase {
+
+	/**
+	 * Example test
+	 */
+	public function testExample() {
+		$this->assertTrue( true );
+	}
+}
